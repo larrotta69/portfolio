28cc73e795ab3938d26eba435757655c72d99715
larrotta69 <larrotta69@gmail.com>
2015-04-08 09:21:39 -0500

    no skrollr library

--- base/js/main.jsx
+++ base/js/main.jsx
@@ -4,19 +4,19 @@ var $ = require('jquery-browserify');
 var foundation = require('./foundation.min.js');
 var velocity = require('./vendor/velocity.min.js');
 var sr = require('./vendor/scrollReveal.min.js');
-var skrollr = require('./vendor/skrollr.min.js');
+//var skrollr = require('./vendor/skrollr.min.js');
 
 
 
 /*mine*/
 var App = require('./app.jsx');
 
-
-
 React.render(<App/>, document.getElementById('main'));
 $(document).foundation();
-window.sr = new scrollReveal({});
-var s = skrollr.init();
+window.sr = new scrollReveal({
+	mobile: true
+});
+//var s = skrollr.init();
 //console.log(s);
 //React.render(<Tiger/>, document.getElementById('main'));
 
--- base/js/tiger.jsx
+++ base/js/tiger.jsx
@@ -7,8 +7,8 @@ var Tiger = React.createClass({
 			<div>
 				<section className="row complete_width cust_tiger">
 					<div className="cust_tiger_div">	
-						<figure className="img_container" data-anchor-target=".cust_tiger_div" data-start="top: 70px; left: 0%;" data-top-bottom="top: 10px; left: -88%;">
-							<img src="app/img/tigre_web.png"  data-anchor-target=".cust_tiger_div" data-start="width: 40%" data-top-bottom="width: 10%"/>	
+						<figure className="img_container" >
+							<img src="app/img/tigre_web.png"/>	
 						</figure>
 					</div>
 					
